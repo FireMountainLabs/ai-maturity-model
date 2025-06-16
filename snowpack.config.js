7747c3e9b1f87668d52b8ad2a32857653cc3a5a4
FireMountainLabs <chris@firemountainlabs.com>
2025-06-15 18:34:24 -0700

    refactor

---
+++ snowpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mount: {
+    public: { url: '/', static: true },
+    src: { url: '/dist' }
+  },
+  buildOptions: {
+    out: 'build',
+    clean: true
+  },
+  routes: [
+    { match: 'routes', src: '.*', dest: '/index.html' }
+  ]
+};
